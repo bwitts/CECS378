3fed5ed0a4febcea41b0ccc6d78b22b1a5ed5e26
bwitts <44324511+bwitts@users.noreply.github.com>
2019-03-06 22:55:56 -0800

    file encryption implemented

--- Lab1.py
+++ Lab1.py
@@ -1,6 +1,10 @@
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding, serialization, hashes, asymmetric
+from cryptography.hazmat.primitives.asymmetric import rsa
+import base64
+import cryptography
 
 #Generate a 16 Bytes IV, and encrypt the message using the key and IV in CBC mode (AES).
 # You return an error if the len(key) < 32 (i.e., the key has to be 32 bytes= 256 bits).
@@ -11,10 +15,12 @@ def myEncrypt(message, key):
     #checking key length, must be 32 bytes
     if len(key) < 32:
         return "Invalid key length."
-    else:
-        #encode message
-        message = message.encode()
-
+    try:
+        #encode message to binary raw data
+        message=message.encode()
+    except:
+        pass
+    
     #initilize 16 bytes IV with secure crytography random generator in os
     IV = os.urandom(16)
 
@@ -55,7 +61,8 @@ def myDecrypt(C, IV, key):
         return plainTxt
     except:
         return plainTxt
-
+    
+#---------------------------------------------------------------------------
 #testing
 key = os.urandom(32) #generate a random 32 bit key
 m = "Brittney" #message you want to encypt then decrypt
@@ -65,3 +72,63 @@ print(result)
 
 print("\nDecrypted message: ")
 print(myDecrypt(result[0], result[1], key).decode('utf8'))
+#---------------------------------------------------------------------------
+
+#This method takes in a filepath of an image that you want to encrypt
+def MyFileEncryption(filepath):
+    
+    #generate a random 32 bit key
+    key = os.urandom(32)
+
+    #open and read the file as single byte string (becomes message)
+    with open (filepath,'rb') as f:
+        data = f.read()
+
+    #pass the string to myEncrypt and save the cipher text
+    result = myEncrypt(data, key)
+
+    #split the path into root and ext so that root + ext = filepath
+    extention = os.path.splitext(filepath)[1]
+
+    #concatenate key and extention to result
+    result += (key, extention)
+
+    #ask user to input a name for encrypted file
+    enc_filepath = input("Enter a filename for encrypted file output: ")
+
+    #create a writable image
+    image_result = open(enc_filepath + extention, 'wb')
+
+    #write the decoding result
+    image_result.write(result[0])
+    
+    return result
+
+#file decryption algorithm
+def MyFileDecrypt(encrypted_filepath, IV, key, extention):
+    
+    #read the encrypted file and put it in 'data'
+    with open(encrypted_filepath, 'rb') as f:
+        data = f.read()
+
+    #set the decryption file path
+    dec_filepath = input("Enter a filename for decrypted file output: ")
+    
+    #decrypting file as plainTxt
+    plainTxt = myDecrypt(data, IV, key)
+
+    #create a writable image
+    image_result = open(dec_filepath + extention, 'wb')
+    
+    #write the decoding result
+    image_result.write(plainTxt)
+
+#-----------------------------------------------------------------------------
+#testing file encryption
+file_path = os.path.abspath("image.jpg") #the name of the image being encrypted/decrypted
+ct,iv,key,ext = MyFileEncryption(file_path)
+
+print("Starting decryption...")
+input_enc_filepath = input("Enter the filename for previously encrypted file: ")
+MyFileDecrypt(input_enc_filepath + ext,iv,key,ext)
+#---------------------------------------------------------------------------
